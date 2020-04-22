f4fbeeccd0ea0c3ccf7a55cdb815095226e4e9b8
youssri ahmed <youssri.ahmed@cg-eg.com>
2020-04-22 10:06:59 +0200

    reshape hidden_oututs

--- my_answers.py
+++ my_answers.py
@@ -95,29 +95,20 @@ class NeuralNetwork(object):
         # TODO: Output error - Replace this value with your calculations.
         error = y-final_outputs# Output layer error is the difference between desired target and actual output.
 
-        # TODO: Calculate the hidden layer's contribution to the error
-        #hidden_error = np.dot(self.weights_input_to_hidden.T, error)
-        
-        # TODO: Backpropagated error terms - Replace these values with your calculations.
-        #output_error_term = error * final_outputs * (1 - final_outputs)
         output_error_term = error
-        #hidden_error_term = hidden_error * self.activation_function(hidden_outputs) * (1 - self.activation_function(hidden_outputs))
-
-        #hidden_error = np.dot(output_error_term.T, self.weights_input_to_hidden)
-
+        
         hidden_error_term = np.dot(output_error_term, self.weights_hidden_to_output.T)
         #hidden_error_term =  hidden_error * hidden_outputs * (1 - hidden_outputs)
-        hidden_error =np.dot(output_error_term, self.weights_hidden_to_output.T)
+        hidden_error=hidden_error_term * hidden_outputs * (1 - hidden_outputs)
 
         # Weight step (input to hidden)
-        #delta_weights_i_h +=  hidden_error_term * X [:,None]
         delta_weights_i_h += np.dot(X.T, hidden_error_term)
+        hidden_outputs = hidden_outputs.reshape(2,1)
 
         # Weight step (hidden to output)
-        #print("__________ delta_weights_i_h in Backpropagated________",delta_weights_i_h)
-
-        #delta_weights_h_o +=  output_error_term * hidden_outputs[:,None]
-        delta_weights_h_o += np.dot(hidden_outputs.T, output_error_term)
+        print("__________ output_error_term in Backpropagated________",output_error_term.shape)
+        print("__________ hidden_outputs in Backpropagated________",hidden_outputs.T.shape)
+        delta_weights_h_o += np.dot( hidden_outputs,output_error_term.T)
 
         return delta_weights_i_h, delta_weights_h_o
 
